a49616c9904931ab507dd744a16af6302b174933
hunnx27 <hunnx27@gmail.com>
2020-03-17 14:54:32 +0900

    css 수정 AD2구조관련

--- src/components/etc/Ad2.js
+++ src/components/etc/Ad2.js
@@ -3,12 +3,15 @@ import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme =>({
     ad2_wrap:{
+       
+    },
+    ad2_innerWrap:{
+        margin:'0 auto',
+        width:1100,
         position:'relative',
-       '& img':{
-           position:'absolute',
-           top:0,
-           left:0
-       }
+        '& img':{
+            width:'100%'
+        }
     },
     ad2_bg:{
         height:37,
@@ -19,6 +22,15 @@ const styles = theme =>({
         margin:'0 auto',
         textAlign:'center',
         borderBottom:'1px solid #efefef'
+    },
+    comment:{
+        position:'absolute',
+        top:30,
+        width:1100,
+        left:0,
+        textAlign:'center',
+        fontWeight:'bolder',
+        fontSize:20
     }
 })
 class Ad2 extends Component{
@@ -26,9 +38,15 @@ class Ad2 extends Component{
         const { classes } = this.props;
         return(
             <div className={classes.ad2_wrap}>
-                <p className={classes.ad2_bg}></p>
-                <p className={classes.ad2_content}>광고2 영역</p>
-                <img src="https://placeimg.com/1400/80/3"/>
+                <div className={classes.ad2_bg}></div>
+                <div className={classes.ad2_innerWrap}>
+                    <ul>
+                        <li>
+                            <img src="https://placeimg.com/1100/80/3"/>
+                        </li>
+                    </ul>
+                    <p className={classes.comment}>광고2 영역</p>
+                </div>
             </div>
         )
     }

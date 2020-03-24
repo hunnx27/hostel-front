192c8ed5affc9ebc4922ec5de766e2243259df3e
hunnx27 <hunnx27@gmail.com>
2020-03-25 00:15:43 +0900

    Add Get Customers REST API Modules

--- hostel-front/src/components/contents/ContentWrap.js
+++ hostel-front/src/components/contents/ContentWrap.js
@@ -22,112 +22,76 @@ const styles = theme => ({
     }
 })
 
-const boards = [
-    {
-      'id':1,
-      'image': 'https://placeimg.com/200/140/1',
-      'productNo': '51-4149',
-      'region': '연천군',
-      'title': '모텔임대합니다. 임대 서울1시간거리',
-      'subtitle': '저렴한 투자비@21객실',
-      'guaranteePrice': '2000',
-      'monthPrice': '200',
-      'wishRightPrice': '1800',
-      'totalPrice': '3800',
-      'realProdChkday': '2019-10-29 14:12',
-      'viewCnt': '337',
-      'sellerName': '서도인',
-      'sellerPhone': '010-1234-1234'
-    },
-    {
-      'id':2,
-      'image': 'https://placeimg.com/200/140/2',
-      'productNo': '51-4149',
-      'region': '서울 종로구',
-      'title': '종로구 게스트하우스',
-      'subtitle': '자리만 마음에 드시면 권리금 협상해드립니다!',
-      'guaranteePrice': '2000',
-      'monthPrice': '200',
-      'wishRightPrice': '1800',
-      'totalPrice': '3800',
-      'realProdChkday': '2019-10-29 14:12',
-      'viewCnt': '238',
-      'sellerName': '최민수',
-      'sellerPhone': '010-1234-1234'
-    },
-    {
-      'id':3,
-      'image': 'https://placeimg.com/200/140/3',
-      'productNo': '51-4149',
-      'region': '인천 미추홀구',
-      'title': '유흥가 중심상권 모텔 임대',
-      'subtitle': '백악관나이트 한국관나이트등 유흥가 중심상권 모텔',
-      'guaranteePrice': '5000',
-      'monthPrice': '440',
-      'wishRightPrice': '1000',
-      'totalPrice': '6000',
-      'realProdChkday': '2019-10-29 14:12',
-      'viewCnt': '337',
-      'sellerName': '김수미',
-      'sellerPhone': '010-1234-1234'
-    }
-  ]
-
 class ContentWrap extends Component{
-    render(){
-        const { classes } = this.props;
-        return(
-            <div className={classes.wrap}>
-              <div className={classes.innerWrap}>
-                <Table className={classes.table}>
-                  <TableHead>
-                      <TableRow>
-                      <TableCell>식별ID</TableCell>
-                      <TableCell>이미지</TableCell>
-                      <TableCell>매물번호</TableCell>
-                      <TableCell>지역</TableCell>
-                      <TableCell>제목</TableCell>
-                      <TableCell>서브제목</TableCell>
-                      <TableCell>보증금</TableCell>
-                      <TableCell>월세</TableCell>
-                      <TableCell>희망권리금</TableCell>
-                      <TableCell>합계</TableCell>
-                      <TableCell>실매물확인일</TableCell>
-                      <TableCell>조회수</TableCell>
-                      <TableCell>매도인</TableCell>
-                      <TableCell>휴대폰</TableCell>
-                      </TableRow>
-                  </TableHead>
-                  <TableBody>
-                  {
-                      boards.map(c => {
-                      return (
-                          <Board
-                          key={c.id}
-                          id={c.id}
-                          image={c.image}
-                          productNo={c.productNo}
-                          region={c.region}
-                          title={c.title}
-                          subtitle={c.subtitle}
-                          guaranteePrice={c.guaranteePrice}
-                          monthPrice={c.monthPrice}
-                          wishRightPrice={c.wishRightPrice}
-                          totalPrice={c.totalPrice}
-                          realProdChkday={c.realProdChkday}
-                          viewCnt={c.viewCnt}
-                          sellerName={c.sellerName}
-                          sellerPhone={c.sellerPhone}
-                          />
-                      )
-                      })
-                  }
-                  </TableBody>
-                </Table>
-                </div>
-            </div>
-        )
-    }
+  state = {
+    products: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({products: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('api/products');
+    const body = await response.json();
+    return body;
+  }
+  render(){
+      const { classes } = this.props;
+      return(
+          <div className={classes.wrap}>
+            <div className={classes.innerWrap}>
+              <Table className={classes.table}>
+                <TableHead>
+                    <TableRow>
+                    <TableCell>식별ID</TableCell>
+                    <TableCell>이미지</TableCell>
+                    <TableCell>매물번호</TableCell>
+                    <TableCell>지역</TableCell>
+                    <TableCell>제목</TableCell>
+                    <TableCell>서브제목</TableCell>
+                    <TableCell>보증금</TableCell>
+                    <TableCell>월세</TableCell>
+                    <TableCell>희망권리금</TableCell>
+                    <TableCell>합계</TableCell>
+                    <TableCell>실매물확인일</TableCell>
+                    <TableCell>조회수</TableCell>
+                    <TableCell>매도인</TableCell>
+                    <TableCell>휴대폰</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                {
+                    this.state.products ? this.state.products.map(c => {
+                    return (
+                        <Board
+                        key={c.id}
+                        id={c.id}
+                        image={c.image}
+                        productNo={c.productNo}
+                        region={c.region}
+                        title={c.title}
+                        subtitle={c.subtitle}
+                        guaranteePrice={c.guaranteePrice}
+                        monthPrice={c.monthPrice}
+                        wishRightPrice={c.wishRightPrice}
+                        totalPrice={c.totalPrice}
+                        realProdChkday={c.realProdChkday}
+                        viewCnt={c.viewCnt}
+                        sellerName={c.sellerName}
+                        sellerPhone={c.sellerPhone}
+                        />
+                    )
+                    }) : ""
+                }
+                </TableBody>
+              </Table>
+              </div>
+          </div>
+      )
+  }
 }
 
 export default withStyles(styles)(ContentWrap);
--- server.js
+++ server.js
@@ -6,8 +6,57 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req,res) => {
-    res.send({message: 'hello Express!'});
+app.get('/api/products', (req,res) => {
+    res.send([
+        {
+          "id":1,
+          "image": "https://placeimg.com/200/140/1",
+          "productNo": "51-4149",
+          "region": "연천군",
+          "title": "모텔임대합니다. 임대 서울1시간거리",
+          "subtitle": "저렴한 투자비@21객실",
+          "guaranteePrice": "2000",
+          "monthPrice": "200",
+          "wishRightPrice": "1800",
+          "totalPrice": "3800",
+          "realProdChkday": "2019-10-29 14:12",
+          "viewCnt": "337",
+          "sellerName": "서도인",
+          "sellerPhone": "010-1234-1234"
+        },
+        {
+          "id":2,
+          "image": "https://placeimg.com/200/140/2",
+          "productNo": "51-4149",
+          "region": "서울 종로구",
+          "title": "종로구 게스트하우스",
+          "subtitle": "자리만 마음에 드시면 권리금 협상해드립니다!",
+          "guaranteePrice": "2000",
+          "monthPrice": "200",
+          "wishRightPrice": "1800",
+          "totalPrice": "3800",
+          "realProdChkday": "2019-10-29 14:12",
+          "viewCnt": "238",
+          "sellerName": "최민수",
+          "sellerPhone": "010-1234-1234"
+        },
+        {
+          "id":3,
+          "image": "https://placeimg.com/200/140/3",
+          "productNo": "51-4149",
+          "region": "인천 미추홀구",
+          "title": "유흥가 중심상권 모텔 임대",
+          "subtitle": "백악관나이트 한국관나이트등 유흥가 중심상권 모텔",
+          "guaranteePrice": "5000",
+          "monthPrice": "440",
+          "wishRightPrice": "1000",
+          "totalPrice": "6000",
+          "realProdChkday": "2019-10-29 14:12",
+          "viewCnt": "337",
+          "sellerName": "김수미",
+          "sellerPhone": "010-1234-1234"
+        }
+      ]);
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
